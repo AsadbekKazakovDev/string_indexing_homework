46c2d606669662462a5e8c3f61b13a0f33b559da
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-24 23:00:13 +0500

    5-masala yechimi

--- Index05.py
+++ Index05.py
@@ -1,3 +1,4 @@
+from math import*
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +7,8 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    return s[0].isdigit()+s[1].isdigit()+s[2].isdigit()+s[3].isdigit()+s[4].isdigit()
+s = "32xz2"
+print(main(s))
+s = "1934z"
+print(main(s))
